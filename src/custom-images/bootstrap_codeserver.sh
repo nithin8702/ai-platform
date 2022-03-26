2c752bba1ae6f92f625d1fa2518248f2f6367be9
bitspilanigroup2 <bitspilanigroup2@gmail.com>
2022-03-26 08:06:14 +0530

    codeserver image

---
+++ src/custom-images/bootstrap_codeserver.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+
+code-server --bind-addr 0.0.0.0 --port 8888 --auth none --verbose /app
+
---
+++ src/custom-images/bootstrap_jupyter.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+
+jupyter lab --notebook-dir=/app --ip=0.0.0.0 --no-browser --allow-root --port=8888 --NotebookApp.token='' --NotebookApp.password='' --NotebookApp.allow_origin='*' --NotebookApp.base_url="${NB_PREFIX}"
